702f17d440798f5c6b98b1b26aaf0a3aecb4d9c4
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-06-27 13:33:42 +0530

    Time: 2 ms (35.78%) | Memory: 42 MB (71.01%) - LeetSync

--- 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.java
@@ -3,10 +3,23 @@ class Solution {
         // Approach 1: Frequency Array + Odd/Even Check (Best)
         // Why Best? Input size and number range (0 ≤ nums[i] ≤ 100) are small.
         // Time: O(n), Space: O(1) (fixed-size array)
-        int[] freq = new int[101];
-        for (int num : nums) freq[num]++;
-        int pairs = 0, leftovers= 0;
-        for (int count : freq) {
+        // int[] freq = new int[101];
+        // for (int num : nums) freq[num]++;
+        // int pairs = 0, leftovers= 0;
+        // for (int count : freq) {
+        //     pairs += count / 2;
+        //     leftovers += count % 2;
+        // }
+        // return new int[]{pairs, leftovers};
+
+
+        // Approach 2: HashMap + Odd/Even Counting
+        // Useful if the number range wasn't fixed.
+        // Time: O(n), Space: O(n)
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) map.put(num, map.getOrDefault(num, 0) + 1);
+        int pairs = 0, leftovers = 0;
+        for (int count : map.values()) {
             pairs += count / 2;
             leftovers += count % 2;
         }
