f4423940c8538d6726c84d7e30792d73f1f93456
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-09-08 15:04:38 +0530

    Time: 2 ms (98.80%) | Memory: 43.1 MB (75.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isPalindrome(String s) {
         // // Optimal Approach using Two-pointers (Time: O(n), Space: O(1))
-        // int left = 0, right = s.length() - 1;
-        // while (left < right) {
-        //     // Move left pointer until it's alphanumeric
-        //     while (left < right && !Character.isLetterOrDigit(s.charAt(left)))
-        //     left++;
-        //     // Move right pointer until it's alphanumeric
-        //     while (left < right && !Character.isLetterOrDigit(s.charAt(right)))
-        //     right--;
-        //     // Compare ignoring case
-        //     if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
-        //     return false;
-        //     left++;
-        //     right--;
-        // }
-        // return true;
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            // Move left pointer until it's alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left)))
+            left++;
+            // Move right pointer until it's alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right)))
+            right--;
+            // Compare ignoring case
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right)))
+            return false;
+            left++;
+            right--;
+        }
+        return true;
 
         // // Regex + String Builder (simple, but Time: O(n), Space: O(n))
         // // Remove all non-alphanumeric characters & convert to lowercase
@@ -25,14 +25,14 @@ class Solution {
         // // Compare original cleaned with reversed
         // return cleaned.equals(reversed);
 
-        // Regex + 2-pointer (Time: 0(n), Space: O(n))
-        s = s.toLowerCase();
-        s = s.replaceAll("[^a-z0-9]", "");
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - 1 - i))
-            return false;
-        }
-        return true;
+        // // Regex + 2-pointer (Time: 0(n), Space: O(n))
+        // s = s.toLowerCase();
+        // s = s.replaceAll("[^a-z0-9]", "");
+        // int n = s.length();
+        // for (int i = 0; i < n / 2; i++) {
+        //     if (s.charAt(i) != s.charAt(n - 1 - i))
+        //     return false;
+        // }
+        // return true;
     }
 }
