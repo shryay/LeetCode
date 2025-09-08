051a757aae720d712cbd361a35afe315884ca82c
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-09-08 16:43:53 +0530

    Time: 2 ms (98.69%) | Memory: 57.9 MB (56.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,15 +1,31 @@
 class Solution {
     public int maxArea(int[] height) {
-        // Simple Two-Pointer Approach
+        // // Simple Two-Pointer Approach
+        // int i = 0, j = height.length - 1;
+        // int maxWater = 0;
+        // while (i < j) {
+        //     int h = Math.min(height[i], height[j]);
+        //     int w = j - i;
+        //     maxWater = Math.max(maxWater, h * w);
+        //     if (height[i] < height[j])
+        //     i++;
+        //     else
+        //     j--;
+        // }
+        // return maxWater;
+
+
+        // Approach 2: Skip Smaller Heights Faster
         int i = 0, j = height.length - 1;
         int maxWater = 0;
         while (i < j) {
             int h = Math.min(height[i], height[j]);
             int w = j - i;
             maxWater = Math.max(maxWater, h * w);
-            if (height[i] < height[j])
+            // Skip all heights smaller than current h
+            while (i < j && height[i] <= h) 
             i++;
-            else
+            while (i < j && height[j] <= h)
             j--;
         }
         return maxWater;
