71cfd285d79c4de0881ed02b5852c067d1c0616a
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-09-08 17:29:08 +0530

    Time: 2 ms (79.55%) | Memory: 46.2 MB (68.92%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -14,6 +14,7 @@ class Solution {
         // }
 
 
+        // Approach 2: Swapping Version (slightly trickier)
         int lastNonZero = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
