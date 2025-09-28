af250f574e3db5da6ac29123725996cd7c4d11e5
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-09-28 11:05:16 +0530

    Time: 29 ms (86.82%) | Memory: 56 MB (99.45%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,47 +1,80 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        int n = nums.length;
         
+    //     // Approach-1 (Using Binary Search)
+    //     // T.C : O(nlogn)
+    //     // S.C : O(n) for prefixSum for effienctly calculating windowSum
+
+    //     int n = nums.length;
+        
+    //     Arrays.sort(nums);
+    //     long[] prefixSum = new long[n];
+    //     prefixSum[0] = nums[0];
+        
+    //     for (int i = 1; i < n; i++) {
+    //         prefixSum[i] = prefixSum[i - 1] + nums[i];
+    //     }
+        
+    //     int result = 0;
+        
+    //     for (int i = 0; i < n; i++) {
+    //         result = Math.max(result, findBest(i, k, nums, prefixSum));
+    //     }
+        
+    //     return result;
+    // }
+    // private int findBest(int targetIdx, int k, int[] nums, long[] prefixSum) {
+    //     int target = nums[targetIdx];
+        
+    //     int i = 0;
+    //     int j = targetIdx;
+    //     int result = targetIdx;
+        
+    //     while (i <= j) {
+    //         int mid = i + (j - i) / 2;
+            
+    //         long count = (targetIdx - mid + 1);
+    //         long windowSum = (count * target);
+    //         long currSum = prefixSum[targetIdx] - prefixSum[mid] + nums[mid];
+            
+    //         int ops = (int) (windowSum - currSum);
+            
+    //         if (ops > k) {
+    //             i = mid + 1;
+    //         } else {
+    //             result = mid;
+    //             j = mid - 1;
+    //         }
+    //     }
+        
+    //     return targetIdx - result + 1;
+
+
+        // Approach-2 (Using sliding window)
+        // T.C : O(nlogn)
+        // S.C : O(1)
         Arrays.sort(nums);
-        long[] prefixSum = new long[n];
-        prefixSum[0] = nums[0];
         
-        for (int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-        }
+        int n = nums.length;
         
         int result = 0;
         
-        for (int i = 0; i < n; i++) {
-            result = Math.max(result, findBest(i, k, nums, prefixSum));
-        }
-        
-        return result;
-    }
-    private int findBest(int targetIdx, int k, int[] nums, long[] prefixSum) {
-        int target = nums[targetIdx];
-        
         int i = 0;
-        int j = targetIdx;
-        int result = targetIdx;
+        long currSum = 0;
         
-        while (i <= j) {
-            int mid = i + (j - i) / 2;
+        for (int j = 0; j < n; j++) {
             
-            long count = (targetIdx - mid + 1);
-            long windowSum = (count * target);
-            long currSum = prefixSum[targetIdx] - prefixSum[mid] + nums[mid];
+            long target = nums[j];
+            currSum += nums[j];
             
-            int ops = (int) (windowSum - currSum);
-            
-            if (ops > k) {
-                i = mid + 1;
-            } else {
-                result = mid;
-                j = mid - 1;
+            while ((j - i + 1) * target - currSum > k) {
+                currSum -= nums[i];
+                i++;
             }
+            
+            result = Math.max(result, j - i + 1);
+            
         }
-        
-        return targetIdx - result + 1;
+        return result;
     }
 }
