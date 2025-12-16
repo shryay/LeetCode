b26ad08bd5ea37d6b91b4c515f8234e778db91bd
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-12-16 22:05:53 +0530

    Time: 1 ms (99.33%) | Memory: 44.4 MB (69.71%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -1,18 +1,27 @@
 class Solution {
+    // public int findNumbers(int[] nums) {
+    //     int evenCount = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         if (hasEvenNumberOfDigits(nums[i]))
+    //             evenCount++;
+    //     }
+    //     return evenCount++;
+    // }
+    // private boolean hasEvenNumberOfDigits(int num) {
+    //     int digitCount = 0;
+    //     while (num != 0) {
+    //         num = num / 10;
+    //         digitCount++;
+    //     }
+    //     return digitCount % 2 == 0;
+    // }
+
     public int findNumbers(int[] nums) {
         int evenCount = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (hasEvenNumberOfDigits(nums[i]))
-                evenCount++;
+        for (int num : nums) {
+            if ((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999) || num == 100000)
+            evenCount++;
         }
-        return evenCount++;
-    }
-    private boolean hasEvenNumberOfDigits(int num) {
-        int digitCount = 0;
-        while (num != 0) {
-            num = num / 10;
-            digitCount++;
-        }
-        return digitCount % 2 == 0;
+        return evenCount;
     }
 }
