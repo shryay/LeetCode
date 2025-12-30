630abb79db06e1ba60c36384fb574781baedc037
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2025-12-30 22:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (64.20%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.java
+++ 870-magic-squares-in-grid/magic-squares-in-grid.java
@@ -5,32 +5,35 @@ class Solution {
         int count = 0;
         for (int i = 0; i <= rows - 3; i++) {
             for (int j = 0; j <= cols - 3; j++) {
-                if (isMagicSquare(grid, i, j)) {
+                if (isMagic(grid, i, j)) {
                     count++;
                 }
             }
         }
         return count;
     }
-    private boolean isMagicSquare(int[][] grid, int r, int c) {
-        boolean[] unique = new boolean[10];
+    private boolean isMagic(int[][] g, int r, int c) {
+        if (g[r + 1][c + 1] != 5) return false;
+        boolean[] seen = new boolean[10];
         for (int i = r; i < r + 3; i++) {
             for (int j = c; j < c + 3; j++) {
-                int val = grid[i][j];
-                if (val < 1 || val > 9 || unique[val]) {
-                    return false;
-                }
-                unique[val] = true;
+                int val = g[i][j];
+                if (val < 1 || val > 9 || seen[val]) return false;
+                seen[val] = true;
             }
         }
-        int sum = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
-        return (grid[r][c] + grid[r][c + 1] + grid[r][c + 2] == sum &&
-                grid[r + 1][c] + grid[r + 1][c + 1] + grid[r + 1][c + 2] == sum &&
-                grid[r + 2][c] + grid[r + 2][c + 1] + grid[r + 2][c + 2] == sum &&
-                grid[r][c] + grid[r + 1][c] + grid[r + 2][c] == sum &&
-                grid[r][c + 1] + grid[r + 1][c + 1] + grid[r + 2][c + 1] == sum &&
-                grid[r][c + 2] + grid[r + 1][c + 2] + grid[r + 2][c + 2] == sum &&
-                grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] == sum &&
-                grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] == sum);
+        for (int i = 0; i < 3; i++) {
+            if (g[r + i][c] + g[r + i][c + 1] + g[r + i][c + 2] != 15)
+                return false;
+        }
+        for (int j = 0; j < 3; j++) {
+            if (g[r][c + j] + g[r + 1][c + j] + g[r + 2][c + j] != 15)
+                return false;
+        }
+        if (g[r][c] + g[r + 1][c + 1] + g[r + 2][c + 2] != 15)
+            return false;
+        if (g[r][c + 2] + g[r + 1][c + 1] + g[r + 2][c] != 15)
+            return false;
+        return true;
     }
 }
