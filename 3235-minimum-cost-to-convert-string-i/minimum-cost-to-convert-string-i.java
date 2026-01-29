a80b332f75f81e68130d648b59efba02ea476afc
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2026-01-29 12:05:54 +0530

    Time: 24 ms (56.27%) | Memory: 47.8 MB (80.29%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -1,36 +1,35 @@
 class Solution {
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        int n = source.length();
-        int numLetters = 26;
-        long[][] minCost = new long[numLetters][numLetters];
-        for (int i = 0; i < numLetters; i++) {
-            Arrays.fill(minCost[i], Long.MAX_VALUE);
-            minCost[i][i] = 0;
+        final int INFINITY = 1 << 29;
+        int[][] graph = new int[26][26];
+        for (int i = 0; i < 26; i++) {
+            Arrays.fill(graph[i], INFINITY);
+            graph[i][i] = 0;
         }
         for (int i = 0; i < original.length; i++) {
-            int from = original[i] - 'a';
-            int to = changed[i] - 'a';
-            minCost[from][to] = Math.min(minCost[from][to], cost[i]);
+            int fromChar = original[i] - 'a';
+            int toChar = changed[i] - 'a';
+            int transformCost = cost[i];
+            graph[fromChar][toChar] = Math.min(graph[fromChar][toChar], transformCost);
         }
-        for (int k = 0; k < numLetters; k++) {
-            for (int i = 0; i < numLetters; i++) {
-                for (int j = 0; j < numLetters; j++) {
-                    if (minCost[i][k] != Long.MAX_VALUE && minCost[k][j] != Long.MAX_VALUE) {
-                        minCost[i][j] = Math.min(minCost[i][j], minCost[i][k] + minCost[k][j]);
-                    }
+        for (int intermediate = 0; intermediate < 26; intermediate++) {
+            for (int start = 0; start < 26; start++) {
+                for (int end = 0; end < 26; end++) {
+                    graph[start][end] = Math.min(graph[start][end], 
+                                                graph[start][intermediate] + graph[intermediate][end]);
                 }
             }
         }
         long totalCost = 0;
-        for (int i = 0; i < n; i++) {
-            if (source.charAt(i) != target.charAt(i)) {
-                int srcChar = source.charAt(i) - 'a';
-                int tgtChar = target.charAt(i) - 'a';
-                long costToConvert = minCost[srcChar][tgtChar];
-                if (costToConvert == Long.MAX_VALUE) {
+        int stringLength = source.length();
+        for (int i = 0; i < stringLength; i++) {
+            int sourceChar = source.charAt(i) - 'a';
+            int targetChar = target.charAt(i) - 'a';
+            if (sourceChar != targetChar) {
+                if (graph[sourceChar][targetChar] >= INFINITY) {
                     return -1;
                 }
-                totalCost += costToConvert;
+                totalCost += graph[sourceChar][targetChar];
             }
         }
         return totalCost;
