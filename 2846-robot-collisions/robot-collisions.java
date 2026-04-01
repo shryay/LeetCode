a5bd27011a8b07ecc910f0e9e7fd91a8ff87e086
Shreya Upadhyay <connect.shreyaupadhyay@gmail.com>
2026-04-01 11:03:37 +0530

    Time: 37 ms (75.65%) | Memory: 101.9 MB (100.00%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -1,54 +1,38 @@
-class Robot {
-  public int index;
-  public int position;
-  public int health;
-  public char direction;
-  public Robot(int index, int position, int health, char direction) {
-    this.index = index;
-    this.position = position;
-    this.health = health;
-    this.direction = direction;
-  }
-}
-
 class Solution {
-  public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
-    List<Integer> ans = new ArrayList<>();
-    Robot[] robots = new Robot[positions.length];
-    List<Robot> stack = new ArrayList<>(); // running robots
-
-    for (int i = 0; i < positions.length; ++i)
-      robots[i] = new Robot(i, positions[i], healths[i], directions.charAt(i));
-
-    Arrays.sort(robots, (a, b) -> a.position - b.position);
-
-    for (Robot robot : robots) {
-      if (robot.direction == 'R') {
-        stack.add(robot);
-        continue;
-      }
-      // Collide with robots going right if any.
-      while (!stack.isEmpty() && stack.get(stack.size() - 1).direction == 'R' && robot.health > 0) {
-        if (stack.get(stack.size() - 1).health == robot.health) {
-          stack.remove(stack.size() - 1);
-          robot.health = 0;
-        } else if (stack.get(stack.size() - 1).health < robot.health) {
-          stack.remove(stack.size() - 1);
-          robot.health -= 1;
-        } else { // stack[-1].health > robot.health
-          stack.get(stack.size() - 1).health -= 1;
-          robot.health = 0;
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        Integer[] indices = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            indices[i] = i;
         }
-      }
-      if (robot.health > 0)
-        stack.add(robot);
+        Arrays.sort(indices, (i, j) -> Integer.compare(positions[i], positions[j]));
+        Stack<Integer> stack = new Stack<>();
+        for (int currentIndex : indices) {
+            if (directions.charAt(currentIndex) == 'R') {
+                stack.push(currentIndex);
+            } else {
+                while (!stack.isEmpty() && healths[currentIndex] > 0) {
+                    int rightMovingRobotIndex = stack.pop();
+                    if (healths[rightMovingRobotIndex] > healths[currentIndex]) {
+                        healths[rightMovingRobotIndex] -= 1;
+                        healths[currentIndex] = 0;
+                        stack.push(rightMovingRobotIndex);
+                    } else if (healths[rightMovingRobotIndex] < healths[currentIndex]) {
+                        healths[currentIndex] -= 1;
+                        healths[rightMovingRobotIndex] = 0;
+                    } else {
+                        healths[currentIndex] = 0;
+                        healths[rightMovingRobotIndex] = 0;
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int health : healths) {
+            if (health > 0) {
+                result.add(health);
+            }
+        }
+        return result;
     }
-
-    stack.sort((a, b) -> a.index - b.index);
-
-    for (Robot robot : stack)
-      ans.add(robot.health);
-
-    return ans;
-  }
 }
